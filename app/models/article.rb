6df00a8a55c126178c81ffabecb2cc0949d16043
[tatsuyakamada] <[t.kamada1151@gmail.com]>
2019-04-11 09:22:10 +0900

    articles_seed_data

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,5 @@
 class Article < ApplicationRecord
-
+  validates :title, :body, :relesed_at, presence: true
+  validates :title, length: {maximum: 80}
+  validates :body, length: {maximum: 2000}
 end
