cc6c64353fe9ec9675fc93d7e5127110388242e4
[tatsuyakamada] <[t.kamada1151@gmail.com]>
2019-04-13 15:59:40 +0900

    9.3

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,32 @@ class Article < ApplicationRecord
   validates :title, :body, :relesed_at, presence: true
   validates :title, length: {maximum: 80}
   validates :body, length: {maximum: 2000}
-end
+
+  def no_expiration
+    expired_at.nil?
+  end
+
+  def no_expiration=(val)
+    @no_expiration = val.in?([true, "1"])
+  end
+
+  before_validation do
+    self.expired_at = nil if @no_expiration
+  end
+
+  validate do
+    if expired_at && expired_at < relesed_at
+      errors.add(:expired_at, :expired_at_too_old)
+    end
+  end
+
+
+  scope :open_to_the_public, -> { where(member_only: false) }
+
+  scope :visible, -> do
+    now = Time.current
+
+    where("relesed_at <= ?", now)
+      .where("expired_at > ? OR expired_at IS NULL", now)
+    end
+  end
